ccd09bf3d6275b35d88223475b9e314c548df490
Anjali <124015584+annisigh@users.noreply.github.com>
2024-03-06 22:59:51 +0530

    adding frontend files

---
+++ src/data.js
@@ -0,0 +1,8 @@
+let moviesList = ['Barfi', 'Hi papa', 'Tamasha', 'Chennai Express', 'Rockstar', 'Queen', 'Leo', 'Friends', 'Jersey'];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
+
+
